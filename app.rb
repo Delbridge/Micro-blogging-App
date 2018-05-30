3f97b62a4b3746b8b3614185be3f5a41c2aa573d
AMKlein2018 <adamklein0519@gmail.com>
2018-05-30 12:18:28 -0400

    more changes

--- app.rb
+++ app.rb
@@ -1,5 +1,30 @@
 require 'sinatra'
 require 'sinatra/activerecord' 
 require './models'   
-set :database, 'sqlite3:books.sqlite3'  
+set :database, 'sqlite3:books.sqlite3'
+set :sessions, true
+
+# Login functionality
+
+get "/login" do
+
+	erb :'users/login'
+end
+
+post "login" do
+	user = User.where(username: params[:username]).first
+	if user.password == params[:password]
+		session[:user_id] = user.user_id
+		redirect "/userprofilenew"
+	else
+		redirect "/login"
+	end
+end
+
+get "/userprofilenew" do
+
+	erb :'useprofilenew'
+
+end
+
 
