484b992b2ab11ca6b082a17ec6f9d6bd1ba2eb36
Upshur06 <mupshur2005@gmail.com>
2018-05-30 11:47:45 -0400

    workingon

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require 'sinatra'
-require 'sinatra/activerecord' 
-require './models'   
-set :database, 'sqlite3:books.sqlite3'  
+require 'sinatra/activerecord'
+require './models'
+set :database, 'sqlite3:books.sqlite3'
+
+
+# Signup action
+ get "/signup" do
+   erb :"users/new"
+ end
+
+    post "/create" do
+      User.create(username: parmas[:username], password: params[:password])
+      redirect "/"
+    end
